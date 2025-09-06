c6011c5cad75bd2cb551c00bbd51328a7b2186ab
TheRudraBro <rudramraj22@gmail.com>
2025-09-06 17:41:06 +0600

    Updated

---
+++ test.js
@@ -0,0 +1,12 @@
+
+
+const data=async()=>{
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const json = await response.json();
+    console.log(json);
+
+      console.log('Hello');
+      console.log(true);
+}
+data();
